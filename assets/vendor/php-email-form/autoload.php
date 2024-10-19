bb9872cdfdd7b947b153cc3476c72f4f036290db
Md. Jahid Hasan <92420911+Roohjahidhasan@users.noreply.github.com>
2024-10-20 01:08:53 +0600

    Update autoload.php

--- assets/vendor/php-email-form/autoload.php
+++ assets/vendor/php-email-form/autoload.php
@@ -1,15 +1,18 @@
 <?php
 require 'vendor/php-email-form/autoload.php';
 
-<form action="https://formspree.io/f/mkgnndkv" method="POST">
+<form action="https://formspree.io/f/mkgnndkv" method="POST" data-redirect="/thanks">
   <label>
     Your email:
     <input type="email" name="email" required>
   </label>
+  <br>
   <label>
     Your message:
     <textarea name="message" required></textarea>
   </label>
+  <br>
   <button type="submit">Send</button>
 </form>
+
 ?>

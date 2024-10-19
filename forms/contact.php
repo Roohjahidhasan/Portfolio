ecb94becc0b2a86573d49e091a3cd3294d485bc2
Md. Jahid Hasan <92420911+Roohjahidhasan@users.noreply.github.com>
2024-10-19 21:37:40 +0600

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,33 +1,11 @@
-<?php
- require 'vendor/autoload.php';
-  action="https://formspree.io/f/mkgnndkv"
-  method="POST"
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form inputs
-    $name = htmlspecialchars(trim($_POST['name']));
-    $email = htmlspecialchars(trim($_POST['email']));
-    $subject = htmlspecialchars(trim($_POST['subject']));
-    $message = htmlspecialchars(trim($_POST['message']));
-
-    // Email to which the form data will be sent
-    $to = "roohjahidhasan@yahoo.com";  // Replace with your email address
-
-    // Prepare email headers
-    $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-
-    // Create email content
-    $email_body = "Name: $name\n";
-    $email_body .= "Email: $email\n";
-    $email_body .= "Subject: $subject\n";
-    $email_body .= "Message:\n$message\n";
-
-    // Send email
-    if (mail($to, $subject, $email_body, $headers)) {
-        echo "Thank you for contacting us, $name. We will get back to you soon.";
-    } else {
-        echo "Sorry, there was an error sending your message. Please try again later.";
-    }
-}
-?>
+<form action="https://formspree.io/f/mkgnndkv" method="POST">
+  <label>
+    Your email:
+    <input type="email" name="email" required>
+  </label>
+  <label>
+    Your message:
+    <textarea name="message" required></textarea>
+  </label>
+  <button type="submit">Send</button>
+</form>

7b0ff7fe2abd59a228d81b84d73c6a7423a171be
Md. Jahid Hasan <92420911+Roohjahidhasan@users.noreply.github.com>
2024-10-19 22:22:22 +0600

    Update autoload.php

--- assets/vendor/php-email-form/autoload.php
+++ assets/vendor/php-email-form/autoload.php
@@ -1,16 +1,15 @@
 <?php
 require 'vendor/php-email-form/autoload.php';
 
-$transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
-  ->setUsername('roohzahidhasan')
-  ->setPassword('@Jahid15111998@');
-
-$mailer = new Swift_Mailer($transport);
-
-$message = (new Swift_Message('Hello'))
-  ->setFrom(['roohzahidhasan@gmail.com' => 'John Doe'])
-  ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
-  ->setBody('Here is the message itself');
-
-$result = $mailer->send($message);
+<form action="https://formspree.io/f/mkgnndkv" method="POST">
+  <label>
+    Your email:
+    <input type="email" name="email" required>
+  </label>
+  <label>
+    Your message:
+    <textarea name="message" required></textarea>
+  </label>
+  <button type="submit">Send</button>
+</form>
 ?>
